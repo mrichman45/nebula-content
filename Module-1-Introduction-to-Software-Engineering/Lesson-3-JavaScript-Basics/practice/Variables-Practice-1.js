e6ebbc460e7840857113905ca0fe96b7c613f3e4
mrichman45 <mrichman45@gmail.com>
2024-03-19 14:26:10 -0400

    Variables-Practice-1 Done

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Variables-Practice-1.js
@@ -0,0 +1,27 @@
+/*
+  Create a .js file that satisfies assignment below
+  For each exercise you'll want to log the result to the console to check your work
+*/  
+  
+
+  // 1. Declare a new variable and set it equal to the string of your name
+  let fnameString = "Michael Richman"
+  console.log(fnameString)
+  // 2. Declare a new variable and set it equal to a number 
+   let numberVar = 25
+   console.log(numberVar)
+  // 3. Set a variable to a boolean value 
+  let catsAreAwesome = false
+  // 4. Create a variable called carName, assign the value Volvo to it.
+  let carName = "Volvo"
+  console.log(carName)
+  // 5. Create a variable called num, assign the value 50 to it.
+  let num = 50
+  console.log(num)
+  // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+let num1 = 5
+let num2 = 10
+console.log(num1+num2)
+  // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+let num3 = 15
+console.log(num3=num2+num1)
