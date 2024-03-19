1460512b3885bfc86d33a581e08dc86624c0a036
mrichman45 <mrichman45@gmail.com>
2024-03-19 13:59:12 -0400

    Notes 二千二十四年三月十九日

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/data-type-practice.js
@@ -0,0 +1,15 @@
+//typeof 50
+
+console.log("This is my first log")
+
+console.log(typeof 50);
+// // The above will output "number"
+
+console.log(typeof "here we are")
+// // The above will output "string"
+
+console.log (typeof true)
+// // The above will output "boolean"
+
+
+
---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
@@ -0,0 +1,9 @@
+let myFavoriteShoes = "Adidas";
+
+const birthyear = 1900;
+
+console.log(myFavoriteShoes)
+console.log(birthyear)
+
+const fname= 'Michael'
+console.log(fname)
