79ad3d195110195d8b57ffd1b159c3492bfa19bb
mrichman45 <mrichman45@gmail.com>
2024-03-19 14:46:51 -0400

    Advanced Practice Completed

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Variable-Advanced-Practice.js
@@ -0,0 +1,38 @@
+// 1. Create a variable called "animalName" and assign it the name of your favorite animal as a string.
+const animalName = "Lion"
+console.log(animalName)
+
+// 2. Declare a variable called "numberOfLegs" and assign it the number of legs that your favorite animal has as a number.
+const numberOfLegs = 4
+console.log(numberOfLegs)
+
+// 3. Create a variable called "isCarnivorous" and assign it a boolean value of true or false, depending on whether or not your favorite animal is a carnivore.
+const isCarnivorous = true
+console.log(isCarnivorous)
+
+// 4. Declare a variable called "averageWeight" and assign it the average weight of your favorite animal as a number.
+const averageWeight = 420
+console.log(averageWeight)
+
+// 5. Create a variable called "habitat" and assign it the type of environment in which your favorite animal lives as a string.
+const habitat = "Savannah"
+console.log(habitat)
+
+// 6. Declare a variable called "averageLifespan" and assign it the average lifespan of your favorite animal as a number.
+const averageLifespan = 10
+console.log(averageLifespan)
+
+// 7. Create a variable called "isEndangered" and assign it a boolean value of true or false, depending on whether or not your favorite animal is considered endangered.
+const isEndangered = false
+console.log (isEndangered)
+
+// 8. Declare a variable called "topSpeed" and assign it the top speed of your favorite animal as a number.
+const topSpeed = 46
+console.log(topSpeed)
+
+// 9. Create a variable called "color" and assign it the primary color of your favorite animal as a string.
+const color = "tan"
+console.log (color)
+// 10. Declare a variable called "sound" and assign it the sound that your favorite animal makes as a string.
+const sound = "roar"
+console.log(sound)
--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Variables-Practice-1.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Variables-Practice-1.js
@@ -5,23 +5,24 @@
   
 
   // 1. Declare a new variable and set it equal to the string of your name
-  let fnameString = "Michael Richman"
-  console.log(fnameString)
+  const fname = "Michael-Richman"
+  console.log(fname)
   // 2. Declare a new variable and set it equal to a number 
-   let numberVar = 25
+   const numberVar = 25
    console.log(numberVar)
   // 3. Set a variable to a boolean value 
-  let catsAreAwesome = false
+  const catsAreAwesome = true
+  console.log(catsAreAwesome)
   // 4. Create a variable called carName, assign the value Volvo to it.
-  let carName = "Volvo"
+  const carName = "Volvo"
   console.log(carName)
   // 5. Create a variable called num, assign the value 50 to it.
-  let num = 50
+  const num = 50
   console.log(num)
   // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
-let num1 = 5
-let num2 = 10
+const num1 = 5
+const num2 = 10
 console.log(num1+num2)
   // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
-let num3 = 15
-console.log(num3=num2+num1)
+const num3 = num1+num2
+console.log(num3)
