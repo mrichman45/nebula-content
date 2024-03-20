15b5d999f59d344c68827ecfa6161c81054e1cba
mrichman45 <mrichman45@gmail.com>
2024-03-20 11:51:53 -0400

    Numbers-Practice-2 done

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Numbers-Practice-2.js
@@ -0,0 +1,48 @@
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+// find the square root of 25
+
+// find the value of 3 to the power of 5
+
+// find the value of 7.4 rounded
+
+// find the rounded-up value of 7.4
+
+// find the absolute value of -13
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+
+// create a random number
+
+// Create a random number between 0-10
+
+let q1 = Math.sqrt(25)
+console.log(q1)
+
+let q2 = Math.pow(3,5)
+console.log(q2)
+
+let q3 = Math.round(7.4)
+console.log(q3)
+
+let q4 = Math.ceil(7.4)
+console.log(q4)
+
+let q5 = Math.abs(-13)
+console.log(q5)
+
+let q6 = Math.min(12,13,14,15,2,-1)
+console.log (q6)
+
+let q7 = Math.max(12,13,14,15,2,-1)
+console.log (q7)
+
+let q8 = Math.random();
+console.log(q8)
+
+let q9 = Math.random() * 10;
+console.log(q9)
+
