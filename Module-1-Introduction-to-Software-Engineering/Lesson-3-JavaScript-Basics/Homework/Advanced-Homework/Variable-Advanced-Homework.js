4182e015fc181bc1fb8af2d1714ebe66eff45d38
mrichman45 <mrichman45@gmail.com>
2024-03-20 09:17:48 -0400

    Finished Advanced Homework

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Advanced-Homework/Variable-Advanced-Homework.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Advanced-Homework/Variable-Advanced-Homework.js
@@ -1,28 +1,30 @@
 // 1. Create a variable called "firstName" and assign it the value of your first name as a string.
-
-
+let firstName = "Michael"
+console.log (firstName)
 // 2. Declare a variable called "age" and assign it the value of your age as a number.
-
-
+let age = 30
+console.log(age)
 // 3. Create a variable called "isMarried" and assign it a boolean value of true or false, depending on whether or not you are married.
-
-
+let isMarried = false
+console.log(isMarried)
 // 4. Declare a variable called "favoriteColor" and assign it the value of your favorite color as a string.
-
-
+let favoriteColor = "pink"
+console.log(favoriteColor)
 // 5. Create a variable called "favoriteBook" and assign it the value of your favorite book as a string.
-
-
+let favoriteBook = "Highschool-DXD"
+console.log(favoriteBook)
 // 6. Declare a variable called "numberOfSiblings" and assign it the value of the number of siblings you have as a number.
-
-
+let numberOfSiblings = 1
+console.log(numberOfSiblings)
 // 7. Create a variable called "isStudent" and assign it a boolean value of true or false, depending on whether or not you are currently a student.
-
-
+let isStudent = true
+console.log(isStudent)
 // 8. Declare a variable called "favoriteNumber" and assign it the value of your favorite number as a number.
-
-
+let favoriteNumber = 5
+console.log(favoriteNumber)
 // 9. Create a variable called "favoriteFood" and assign it the value of your favorite food as a string.
-
-
+let favoriteFood = "Pizza"
+console.log(favoriteFood)
 // 10. Declare a variable called "hasPet" and assign it a boolean value of true or false, depending on whether or not you have any pets.
+let hasPet = false
+console.log(hasPet)
