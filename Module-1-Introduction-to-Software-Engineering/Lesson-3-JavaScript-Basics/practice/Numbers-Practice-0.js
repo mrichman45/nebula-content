24f588759d18e87934ef89c822ce2f0af1ffc830
mrichman45 <mrichman45@gmail.com>
2024-03-20 10:27:41 -0400

    Numbers-Practice-0 Finished

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Numbers-Practice-0.js
@@ -0,0 +1,46 @@
+// Log the results to the console
+
+
+// 1. Add together two numbers.
+let add1 = 25
+let add2 = 25
+let add3 = add1+add2
+console.log(add3)
+
+// 2. Subtract two numbers.
+let sub1 = 25
+let sub2 = 10
+let sub3 = sub1-sub2
+console.log(sub3)
+
+// 3. Multiply two numbers.
+let mult1 = 10
+let mult2 = 2
+let mult3 = mult1*mult2
+console.log(mult3)
+
+// 4. Divide two numbers.
+let div1 = 25
+let div2 = 5
+let div3 = div1/div2
+console.log(div3)
+
+// 5. Find the remainder of 223 divided by 3.
+let mod1 = 223
+let mod2 = 3
+let mod3 = mod1%mod2
+console.log(mod3)
+
+// 6. Store the value in a variable produced by dividing 300 by 100. Declare that value with the `let` keyword.
+let quest6pt1 = 300
+let quest6pt2 = 100
+let next2var = quest6pt1/quest6pt2
+console.log(next2var)
+
+// 7. Increment that variable by 1 using the increment operator.
+let num7 = next2var++
+console.log(next2var++)
+
+// 8. Use a multiply assignment operator to change the value of the variable to the product of multiplying it by 5.
+let quest8 = next2var*=
+console.log(next2var*=5)
