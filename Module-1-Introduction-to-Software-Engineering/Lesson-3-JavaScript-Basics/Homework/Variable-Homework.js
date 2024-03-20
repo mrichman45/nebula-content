0701783d8d3cf72d99ca40a23aa0c978c4ca7063
mrichman45 <mrichman45@gmail.com>
2024-03-20 00:47:23 -0400

    Homework Completed

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Variable-Homework.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Variable-Homework.js
@@ -6,3 +6,31 @@
 //1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
 
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+let age = 20
+let height = 48
+let fname = "lion"
+let habitat = "Savannah"
+let mammal = true
+let continent = "Africa"
+
+console.log(age)
+console.log(height)
+console.log(fname)
+console.log(habitat)
+console.log(mammal)
+console.log(continent)
+
+let numberOfWheels = 4
+let Size = "medium"
+let Make = "Nissan"
+let placeOfOriginAmerica = false
+let color = "White"
+let clean = false
+
+console.log(numberOfWheels)
+console.log(Size)
+console.log(Make)
+console.log(placeOfOriginAmerica)
+console.log(color)
+console.log(clean)
