85b816c0b3e61fd1aabe73c603ba264ba9576ade
mrichman45 <mrichman45@gmail.com>
2024-03-21 14:39:37 -0400

    Finished Practice

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Comparison-Operators-Practice.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Comparison-Operators-Practice.js
@@ -8,7 +8,7 @@ console.log(3<2)
 console.log(4<=4.4)
 
 // Reverse the prior statement and check if 4 is greater than or equal to 4.4
-console.log(4<=4.4)
+console.log(4>=4.4)
 
 // Check if the string value of 19 is greater than the numeric value of 2
 console.log("19">3)
--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Equality-Operators-Practice.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Equality-Operators-Practice.js
@@ -1,13 +1,18 @@
 // Create two variables, one equal to '10' and the other equal to 10
-
+const string = "10"
+const number = 10
 
 // Comparing the two variables, are they strictly equal?
-
+console.log(string === number)
 
 // Comparing the two variables, are they loosely equal?
-
+console.log(string == number)
 
 // Check if the variable set to 10 is not loosely equal to 'ten' 
-
+console.log(10 != "ten")
 
 // Create a variable and initialize it to true, check if it is not strictly equal to 'true' 
+
+const Good = true
+
+console.log(Good !== "true")
