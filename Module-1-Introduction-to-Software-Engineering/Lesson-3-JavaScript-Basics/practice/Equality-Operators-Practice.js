5130be5a1eabc32769a021599a45a4083abab642
mrichman45 <mrichman45@gmail.com>
2024-03-21 13:50:32 -0400

    Comparison Practice

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Equality-Operators-Practice.js
@@ -0,0 +1,13 @@
+// Create two variables, one equal to '10' and the other equal to 10
+
+
+// Comparing the two variables, are they strictly equal?
+
+
+// Comparing the two variables, are they loosely equal?
+
+
+// Check if the variable set to 10 is not loosely equal to 'ten' 
+
+
+// Create a variable and initialize it to true, check if it is not strictly equal to 'true' 
