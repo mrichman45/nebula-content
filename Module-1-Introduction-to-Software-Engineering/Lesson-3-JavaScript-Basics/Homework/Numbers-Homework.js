73eacfb4a3d743cbea26817d316f38ca34bee33e
mrichman45 <mrichman45@gmail.com>
2024-03-21 08:20:38 -0400

    Finished Homework for Numbers

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Numbers-Homework.js
@@ -0,0 +1,45 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Remember to console.log() to debug your code and check your solutions!
+
+// Declare a variable and assign it to a numeric value
+let num = 6
+console.log(num)
+// Half that value and re-assign it to the previous variable
+let num2 = 6/3
+console.log(num2)
+// Create two negative numbers, assign them to variables and subtract one from the other together
+let neg1 = -5
+let neg2 = -5
+console.log(neg1-neg2)
+// Create three numbers, assign them to variables and multiply them together
+let mult1 = 5
+let mult2 = 10
+let mult3 = 15
+console.log(mult1*mult2*mult3)
+// Create two even numbers, assign them to variables and divide one from the other
+let even = 20
+let even2= 10
+console.log(even/even2)
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other
+let odd = 7
+let odd2 = 3
+console.log(odd%odd2)
+// find the minimum value in a set of numbers: 10,20,30,40,50
+Math.floor(10,20,30,40,50)
+console.log(Math.floor(10,20,30,40,50))
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5
+Math.ceil(-1,-2,-3,-4,-5)
+console.log(Math.ceil(-1,-2,-3,-4,-5))
+// Create a variable and find it's remainder, if divided by 2
+let vari = 3
+console.log(vari%2)
+// find the square root of 144
+console.log(Math.sqrt(144))
+// find the value of 3 to the power of 7
+console.log(Math.pow(3,7))
+// find the value of 73.2 rounded
+console.log(Math.round(73.2))
+// find the rounded-down value of 9.9
+console.log(Math.floor(9.9))
+// find the absolute value of -1231
+console.log(Math.abs(-1231))
