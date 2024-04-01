74ae97b794a03ae0e8e9b9d258ae663220435e8d
mrichman45 <mrichman45@gmail.com>
2024-03-31 22:11:59 -0400

    I need to go to office hours

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Function-Assignment.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/Homework/Function-Assignment.js
@@ -1,15 +1,21 @@
 // 1. Write a function that takes in an array of numbers and returns a new array containing the numbers with one added to each
 // // Example you pass in the following array [1, 2, 3] the function should return [2, 3, 4]
-
+n/a
 // 2. Write a function that takes in a string, and a letter, and returns true or false if the string contains the letter.
 // // Example string = "happy birthday", letter = "a", should return true
 // // Example string = "happy birthday", letter = "q" should return false
-
+function checkString(bool)
 // 3. Write a function that takes in an array of full names (formatted "firstName lastName"), and returns a new array containing only first names.
 // // Example array = ["Tom Smith", "Bob Jones", "Bill Williams"] returns ["Tom", "Bob", "Bill"]
-
+n/a
 // 4. Write a function that takes in a name and a greeting and logs a string greeting the person to the console.
 // // Example name = "John Boy" greeting = Goodnight, returns "Goodnight John Boy"
-
+N/A
 // 5. Write a function that takes in the year someone was born and returns their age.
 // // Example: birth year: 2000 returns 23
+
+//function ageFinder(num){
+    //console.log(2024-num)
+//}
+
+//ageFinder(31)
